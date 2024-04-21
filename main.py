62ab90d2e594985eb48f079ef28688a0d8672813
Josh Murr <joshuadavidmurr@gmail.com>
2024-04-21 15:59:35 +0100

    cows

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
-from escpos.printer import Usb
+from escpos import config
 
-// TODO: check why this is this way again :/
-printer = Usb(0x0525,0xa700,0)
+CONFIG_LOCATION = "/home/joshmurr/.config/python-escpos"
+c = config.Config()
+c.load(CONFIG_LOCATION)
 
+printer = c.printer()
 printer.open()
-
-printer.text("hey, worl")
-
+printer.image("./cow.jpg")
 printer.cut()
+
+# image(
+#     img_source,
+#     high_density_vertical=True,
+#     high_density_horizontal=True,
+#     impl="bitImageRaster",
+#     fragment_height=960,
+#     center=False,
+# )
+
+# printer.profile.profile_data["media"]["width"]["pixel"] = 128
+# import random
+# MIN = 4000000000001
+# MAX = 4999999999999
+#
+# for i in range(18):
+#     code = random.randint(MIN, MAX)
+#     printer.barcode(f"{code}", "EAN13", 255, 5, "OFF", "")
