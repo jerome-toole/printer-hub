2887fb30b3a318bdb4eee3b1fe43db8f7440a718
Ed Ive <ed.ive@unitetheunion.org>
2024-05-03 16:10:58 +0100

    Refactor main printer script to account for config

--- print.py
+++ print.py
@@ -1,59 +1,79 @@
+"""Main printer script, run with cron"""
+
 from escpos.printer import Usb, LP
 import os
 from dotenv import load_dotenv
-import images
-import web_content
+import requests
+from bs4 import BeautifulSoup
 
 # Load environment variables
 load_dotenv()
 
-# Set options from .env file
-LP_PRINTER_NAME = os.getenv("LP_PRINTER_NAME")
-USB_PRINTER_VENDOR = os.getenv("USB_PRINTER_VENDOR", "0x0525")
-USB_PRINTER_PRODUCT = os.getenv("USB_PRINTER_PRODUCT", "0xa700")
+# Set config options from .env file
+PRINTER_MODE = os.getenv("PRINTER_MODE", "USB")
+LP_PRINTER_NAME = os.getenv("LP_PRINTER_NAME", None)
+# TODO: work out the freaky binary/bytes/int thing going on below
+USB_PRINTER_VENDOR = os.getenv("USB_PRINTER_VENDOR", 0x0525)  # must be bytes
+USB_PRINTER_PRODUCT = os.getenv("USB_PRINTER_PRODUCT", 0xA700)  # def: aures 333
+FEED_URL = os.getenv("FEED_URL", None)
 PRINTER_WIDTH = int(os.getenv("PRINTER_WIDTH", 500))
+LINES_BEFORE = os.getenv("LINES_BEFORE", 0)
+LINES_AFTER = os.getenv("LINES_AFTER", 0)
+CUT_PAPER = os.getenv("CUT_PAPER").lower() == "true"
 
-# Create printer object
-if LP_PRINTER_NAME:
-    printer = LP(LP_PRINTER_NAME)
-else:
-    # Default to a common USB printer if no name is provided
-    # You would need to set the correct vendor and product IDs for your specific printer
-    printer = Usb(USB_PRINTER_VENDOR, USB_PRINTER_PRODUCT, 0)
+# ensure config
+# TODO: manage the config way better, maybe with a TUI tool
 
-# Set up print items
-# img_file = "/Users/jerometoole/Desktop/fax/ed-josh.jpg"
-img_file = None
-txtURL = None
+if PRINTER_MODE.upper() == "LP":
+    try:
+        printer = LP(LP_PRINTER_NAME)
+    except:
+        print("Failed to register LP printer. Check LP_PRINTER_NAME env var")
+        print(
+            "https://python-escpos.readthedocs.io/en/latest/user/usage.html#other-printers"
+        )
+        exit(1)
+elif PRINTER_MODE.upper() == "USB":
+    try:
+
+        USB_PRINTER_VENDOR = bytes(USB_PRINTER_VENDOR)
+        printer = Usb(USB_PRINTER_VENDOR, USB_PRINTER_PRODUCT, 0)
+    except:
+        print("Failed to register USB printer. Check 'USB_*' env vars, using lsusb")
+        print(
+            "https://python-escpos.readthedocs.io/en/latest/user/usage.html#usb-printer"
+        )
+        exit(1)
+
+try:
+    LINES_BEFORE = int(LINES_BEFORE)
+    LINES_AFTER = int(LINES_AFTER)
+except:
+    print("Please ensure that LINES_BEFORE and LINES_AFTER env vars are ints")
+    exit(1)
+
+if FEED_URL == None:
+    print("No FEED_URL env var set")
+    exit(1)
+
+# get content and process
+
+response = requests.get(FEED_URL)
+content = BeautifulSoup(response.content, "html.parser").prettify()
+printer.text(content)
+
+# actually print
 
 printer.open()
 
-if int(os.getenv("LINES_BEFORE", 0)) > 0:
-    for i in range(int(os.getenv("LINES_BEFORE", 0))):
-        printer.text("\n")
+if LINES_BEFORE > 0:
+    printer.text("\n" * LINES_BEFORE)
 
-# Print Content
-
-if txtURL:
-    content = web_content.get_web_content(txtURL, 5)
-
-    printer.text(content)
-
-if img_file:
-    img_file = images.prepare_image(img_file, PRINTER_WIDTH)
-
-    # Print the image
-    printer.image(img_file)
-
-
-# Finish printing
-if int(os.getenv("LINES_AFTER", 0)) > 0:
-    for i in range(int(os.getenv("LINES_AFTER", 0))):
-        printer.text("\n")
-
-print(os.getenv("CUT_PAPER", False))
-
-if os.getenv("CUT_PAPER", False):
+if CUT_PAPER:
     printer.cut()
+
+if LINES_AFTER > 0:
+    printer.text("\n" * LINES_AFTER)
+
 
 printer.close()
